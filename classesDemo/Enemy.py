ddbd712adf5764e276dd17eb2e6e43d7ef726f7e
Aditya Karekar <adityakarekar0311@gmail.com>
2025-03-06 17:24:12 +0530

    added some more content

--- classesDemo/Enemy.py
+++ classesDemo/Enemy.py
@@ -18,4 +18,7 @@ class Enemy:
     def get_type_of_enemy(self):
         return self.__type_of_enemy
     
+    def special_attack(self):
+        print("Enemy haas no special attack")
+    
     
--- classesDemo/Ogre.py
+++ classesDemo/Ogre.py
@@ -1,4 +1,6 @@
 from Enemy import *
+import random
+random
 class Ogre(Enemy):
     def __init__(self, health_points, attack_damage):
         super().__init__(type_of_enemy="Ogre", health_points=health_points, attack_damage=attack_damage)
@@ -7,3 +9,12 @@ class Ogre(Enemy):
         
     def talk(self):
         print("grumbling")
+        
+    def attack(self):
+        print("tossing rocks")
+        
+    def special_attack(self):
+        didSpecialAttackWork=random.random() < 0.2
+        if(didSpecialAttackWork):
+            self.attack_damage+=4
+            print("ogre attack damage increased to 4 points")
