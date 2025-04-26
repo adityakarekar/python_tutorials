ef9f392ea7918ac0f6ccf19ced77e0fd509a0272
Aditya Karekar <adityakarekar0311@gmail.com>
2025-04-26 15:29:49 +0530

    added some more content

---
+++ python venv/main.py
@@ -0,0 +1,8 @@
+from math import sqrt,pi
+from welcome import say_welcome
+
+
+
+say_welcome()
+print(sqrt(16)) # 4.0
+print(pi) # 3.141592653589793
---
+++ python venv/welcome.py
@@ -0,0 +1,5 @@
+def say_welcome() :
+    print("Welcome to the Python virtual environment!")
+    print("This is a simple script to demonstrate the use of a virtual environment.")
+    print("You can run this script after activating the virtual environment.")
+    print("Enjoy coding in Python!")
