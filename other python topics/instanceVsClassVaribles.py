f019ce9de77cc787be20c3016944da68e9187b95
Aditya Karekar <adityakarekar0311@gmail.com>
2025-05-12 14:06:26 +0530

    content added

--- other python topics/instanceVsClassVaribles.py
+++ other python topics/instanceVsClassVaribles.py
@@ -1,14 +1,16 @@
 class Employee:
     # class variable
     company = "Google"
+    no_of_employees = 0
     
     def __init__(self, name,raise_amt):
         # instance variable
+        Employee.no_of_employees += 1
         self.name = name
         self.raise_amt = raise_amt
         
     def show(self):
-        print(f"Name: {self.name}, Company: {self.company}")
+        print(f"Name: {self.name}, Company: {self.company}, company Size: {self.no_of_employees}, Raise Amount: {self.raise_amt}")
 
 
 
